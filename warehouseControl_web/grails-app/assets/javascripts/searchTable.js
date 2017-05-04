caaae7ca5651778c026c075f561cd41ffc76525e
Francisco Reyes <fco271292@gmail.com>
2017-05-04 18:18:09 -0500

    #3 Se ajusta iteracion sobre la tabla

--- warehouseControl_web/grails-app/assets/javascripts/searchTable.js
+++ warehouseControl_web/grails-app/assets/javascripts/searchTable.js
@@ -1,19 +1,33 @@
 function filterItems(idTable,idInputSearch) {
-	
 	var rowTable = document.getElementById(idTable).rows
 	var patterSearchTax = document.getElementById(idInputSearch).value 
 	var row = new Object()
 	var contentCell = ""
 	//console.log("In "+patterSearchTax.value)
-	
 	for(var rowItem = 0; rowItem < rowTable.length; rowItem++){
 		row = rowTable[rowItem].cells
 		for (var columnItem = 0; columnItem < row.length; columnItem++) {
+			//console.log(rowTable[rowItem][columnItem].innerHTML+":::")
 			contentCell = row[columnItem].innerHTML
 			//Se hace la busqueda en la tabla, si se encuentra se presenta la informacion, caso contrario se oculta
-			row[columnItem].style.display = contentCell.toLowerCase().indexOf(patterSearchTax.toLowerCase()) != -1 ? "" : "none"
+			//rowTable[rowItem].style.display = contentCell.toLowerCase().indexOf(patterSearchTax.toLowerCase()) != -1 ? "" : "none"
+			if(contentCell.toLowerCase().indexOf(patterSearchTax.toLowerCase()) != -1){
+				rowTable[rowItem].id = "show"
+			}
+			else{
+				//rowTable[rowItem].id = "row_none"	
+			}
 			
 		}
+		if (rowTable[rowItem].id.toLowerCase().indexOf("show") != -1) {
+			rowTable[rowItem].style.display  = ""
+		}
+		else{
+			rowTable[rowItem].id = "hidden"
+			rowTable[rowItem].style.display  = "none"	
+		}
+
 	}
+
 	
 }
