6caf16d7230e0cd256ed96d9532952b234598606
Francisco Reyes <fco271292@gmail.com>
2017-05-05 13:30:31 -0500

    #3 Se cambia metodo para buscar sobre el contenido de la tabla

--- warehouseControl_web/grails-app/assets/javascripts/searchTable.js
+++ warehouseControl_web/grails-app/assets/javascripts/searchTable.js
@@ -4,28 +4,30 @@ function filterItems(idTable,idInputSearch) {
 	var row = new Object()
 	var contentCell = ""
 	//console.log("In "+patterSearchTax.value)
-	for(var rowItem = 0; rowItem < rowTable.length; rowItem++){
+	for(var rowItem = 1; rowItem < rowTable.length; rowItem++){
+		
+		rowTable[rowItem].id = "table_row"
+		
 		row = rowTable[rowItem].cells
 		for (var columnItem = 0; columnItem < row.length; columnItem++) {
 			//console.log(rowTable[rowItem][columnItem].innerHTML+":::")
 			contentCell = row[columnItem].innerHTML
 			//Se hace la busqueda en la tabla, si se encuentra se presenta la informacion, caso contrario se oculta
 			//rowTable[rowItem].style.display = contentCell.toLowerCase().indexOf(patterSearchTax.toLowerCase()) != -1 ? "" : "none"
-			if(contentCell.toLowerCase().indexOf(patterSearchTax.toLowerCase()) != -1){
+			if(contentCell.toLowerCase().search(patterSearchTax.toLowerCase()) != -1){
 				rowTable[rowItem].id = "show"
-			}
-			else{
-				//rowTable[rowItem].id = "row_none"	
 			}
 			
 		}
-		if (rowTable[rowItem].id.toLowerCase().indexOf("show") != -1) {
+
+		if (rowTable[rowItem].id.toLowerCase().search("show") != -1) {
 			rowTable[rowItem].style.display  = ""
 		}
 		else{
-			rowTable[rowItem].id = "hidden"
+			//rowTable[rowItem].id = "hidden"
 			rowTable[rowItem].style.display  = "none"	
 		}
+		
 
 	}
 
