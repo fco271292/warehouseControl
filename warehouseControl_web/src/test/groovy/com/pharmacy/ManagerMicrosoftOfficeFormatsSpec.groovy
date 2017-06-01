2b301372333b69f01abd037f75a82e0e4acb6e4c
Francisco Reyes <fco271292@gmail.com>
2017-06-01 17:51:44 -0500

    #4 Se ajustan forma de llamar metodo para lectura de excel

--- warehouseControl_web/src/test/groovy/com/pharmacy/ManagerMicrosoftOfficeFormatsSpec.groovy
+++ warehouseControl_web/src/test/groovy/com/pharmacy/ManagerMicrosoftOfficeFormatsSpec.groovy
@@ -8,9 +8,11 @@ import com.pharmacy.ManagerMicrosoftOfficeFormats
 class ManagerMicrosoftOfficeFormatsSpec extends Specification {
 	void "Read book excel" () {
 		given:
+			String pathFile = System.getProperty("user.dir","empty")
+			pathFile = "${pathFile}\\src\\test\\source\\libro.xlsx"
 			ManagerMicrosoftOfficeFormats managerMicrosoftOfficeFormats = new ManagerMicrosoftOfficeFormats()
 		when:
-			managerMicrosoftOfficeFormats.readExcel()
+			managerMicrosoftOfficeFormats.openExcel(pathFile)
 		then:
 			true == false
 	}
